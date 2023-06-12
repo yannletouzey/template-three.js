a612a520593c789f523fe2fa6f69edfd1f0e1775
Yann Letouzey <y.letouzey@gmail.com>
2023-06-12 13:04:57 +0200

    add wireframe

--- src/js/app.js
+++ src/js/app.js
@@ -16,22 +16,34 @@ document.body.appendChild( renderer.domElement );
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.update();
 
-// Create cube geometry
-const geometry = new THREE.BoxGeometry(2, 2, 2 );
-// Create material
-const material = new THREE.MeshNormalMaterial();
-// Create cube with geometry and material
-const cube = new THREE.Mesh( geometry, material );
-// Add cube at scene
-scene.add( cube );
 
-camera.position.z = 5;
+const geometryTorusKnot = new THREE.TorusKnotGeometry( 10, 3, 100, 16  ); 
+// const materialTorusKnot = new THREE.MeshNormalMaterial(); 
+const materialTorusKnot = new THREE.WireframeGeometry( geometryTorusKnot ); 
+// const torusKnot = new THREE.Mesh( geometryTorusKnot, materialTorusKnot ); 
+// scene.add( torusKnot );
+const line = new THREE.LineSegments( materialTorusKnot );
+line.material.depthTest = false;
+// line.material.opacity = 0.25;
+line.material.transparent = true;
+scene.add( line );
+
+// // Create cube geometry
+// const geometry = new THREE.BoxGeometry(2, 2, 2 );
+// // Create material
+// const material = new THREE.MeshNormalMaterial();
+// // Create cube with geometry and material
+// const cube = new THREE.Mesh( geometry, material );
+// // Add cube at scene
+// scene.add( cube );
+
+camera.position.z = 30;
 
 function animate() {
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	line.rotation.x += 0.001;
+	line.rotation.y += 0.001;
 	controls.update();
 	renderer.render( scene, camera );
 }
