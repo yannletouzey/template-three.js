43ab971fde3dbcb04441526065d6fce8799ccb63
Yann Letouzey <y.letouzey@gmail.com>
2023-06-12 12:49:55 +0200

    add orbitControls

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,28 @@
 import * as THREE from 'three';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
+// Create scene
 const scene = new THREE.Scene();
+
+// Create camera
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+// Create renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// Create constrols with mouse
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.update();
+
+// Create cube geometry
+const geometry = new THREE.BoxGeometry(2, 2, 2 );
+// Create material
+const material = new THREE.MeshNormalMaterial();
+// Create cube with geometry and material
 const cube = new THREE.Mesh( geometry, material );
+// Add cube at scene
 scene.add( cube );
 
 camera.position.z = 5;
@@ -19,7 +32,7 @@ function animate() {
 
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
-
+	controls.update();
 	renderer.render( scene, camera );
 }
 
